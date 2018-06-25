4e13d232cd956d29d25849686a016b988540f3bc
mattrudin <matthias.rudin@live.com>
2018-06-25 20:16:34 +0200

    Step 12 to 26

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -10,6 +10,27 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 	return board;
 };
 
-console.log(generatePlayerBoard(3,3));
-console.log(generatePlayerBoard(3,4));
-console.log(generatePlayerBoard(5,3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	for (let i = 0; i < numberOfRows; i++) {
+		let row = [];
+		for (let j = 0; j < numberOfColumns; j++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+	let numberOfBombsPlaced = 0;
+	while (numberOfBombsPlaced < numberOfBombs) {
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		//while loop overrides existing bombs
+	};
+	while (numberOfBombsPlaced < numberOfBombs) {
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+		//while loop overrides existing bombs
+	};
+	board[randomRowIndex][randomColumnIndex] = 'B';
+	numberOfBombsPlaced++ //Is this the right lexical place?
+
+	return board;
+}
+
